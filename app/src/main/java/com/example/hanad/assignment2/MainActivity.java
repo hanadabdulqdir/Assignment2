4f3673f866b229289d821e4eb1939fb1e9fe139a
DESKTOP-7PU8G2V\hanad <hanadabdulqdir@gmail.com>
2018-02-11 15:56:26 +0000

    Update background - card-view

--- app/src/main/java/com/example/hanad/assignment2/MainActivity.java
+++ app/src/main/java/com/example/hanad/assignment2/MainActivity.java
@@ -6,27 +6,19 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hanad.assignment2.classicDetails.model.ClassicMusicFragment;
 import com.example.hanad.assignment2.popDetails.model.PopMusicFragment;
-import com.example.hanad.assignment2.rockDetails.model.Result;
-import com.example.hanad.assignment2.rockDetails.model.RockMusicAdapter;
 import com.example.hanad.assignment2.rockDetails.model.RockMusicFragment;
-import com.example.hanad.assignment2.rockDetails.model.RockMusicList;
-import com.example.hanad.assignment2.service.ServiceConnection;
 
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
 import java.util.List;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,30 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        RockMusicFragment rockMusicFragment = new RockMusicFragment();
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            //FragmentTransaction transaction = fragmentManager.beginTransaction();
-            fragmentManager = getSupportFragmentManager();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_Rock:
                     //mTextMessage.setText(R.string.title_home);
                     Toast.makeText(getApplicationContext(), "Rock", Toast.LENGTH_LONG).show();
+                    fragmentManager = getSupportFragmentManager();
                     fragmentManager.beginTransaction()
                             .add(R.id.fragmentContainer, new RockMusicFragment())
                             .disallowAddToBackStack()
                             .commit();
 
-                    //call the method here to display main
-                    return true;
-                case R.id.navigation_dashboard:
+                    return true; //end of Rock
+                case R.id.navigation_Classic:
+                    fragmentManager = getSupportFragmentManager();
                     Toast.makeText(getApplicationContext(), "Classic", Toast.LENGTH_LONG).show();
                     fragmentManager.beginTransaction()
                             .add(R.id.fragmentContainer, new ClassicMusicFragment())
                             .disallowAddToBackStack()
                             .commit();
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_Pop:
+                    fragmentManager = getSupportFragmentManager();
                     Toast.makeText(getApplicationContext(), "Pop", Toast.LENGTH_LONG).show();
                     fragmentManager.beginTransaction()
                             .add(R.id.fragmentContainer, new PopMusicFragment())
@@ -66,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
                             .commit();
                     return true;
             }
-            return false;
+        return true;
         }
     };
 
@@ -75,19 +68,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //fragmentManager = getSupportFragmentManager();
-        if (savedInstanceState == null) {
+        fragmentManager = getSupportFragmentManager();
+        //if (savedInstanceState == null) {
             RockMusicFragment rockMusicFragment = new RockMusicFragment();
             fragmentManager.beginTransaction()
                     .add(R.id.fragmentContainer, new RockMusicFragment())
                     .addToBackStack(null)
                     .commit();
-
-        } else {
-
-        }
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.getItemBackgroundResource();
+
     }
 }
